dadb16922a5f68bae90c55c73a8ca11f20af7445
Rogelio Morrell <molekilla@gmail.com>
2019-05-31 09:37:53 -0500

    wallet connect interface updates

--- src/IWalletConnectDappProvider.ts
+++ src/IWalletConnectDappProvider.ts
@@ -1,5 +1,7 @@
 import WalletConnect from '@walletconnect/browser'
 import WalletConnectQRCodeModal from '@walletconnect/qrcode-modal'
+import { promisify } from 'util';
+import { fromEvent } from 'rxjs';
 
 export interface IWalletConnectDappProvider {
     processTransaction(txParams, cb: () => {}): Promise<void>;
@@ -10,7 +12,7 @@ export interface IWalletConnectDappProvider {
     getAccounts(cb: () => {}): Promise<void>;
     send(payload, callback): Promise<any>;
     enable(): Promise<any>;
-    getWalletConnector(): Promise<void>;
+    getWalletConnector(): Promise<WalletConnect>;
     start: () => void;
 }
 
@@ -20,58 +22,77 @@ export class WalletConnectImpl implements IWalletConnectDappProvider {
     walletConnector: WalletConnect;
     isWalletConnect: boolean;
 
-    constructor(connector: WalletConnect, options: any) {
+    constructor(options: any) {
         this.qrcode = typeof options.qrcode === 'undefined' || options.qrcode !== false;
         this.bridge = options.bridge || null;
 
-        if (!bridge || typeof bridge !== 'string') {
+        if (!this.bridge || typeof this.bridge !== 'string') {
             throw new Error('Missing or Invalid bridge field')
         }
 
-        this.walletConnector = connector;
+        this.walletConnector = new WalletConnect({ bridge: this.bridge });
+        (this.walletConnector as any).off = () => {};
         this.isWalletConnect = true;
     }
 
     async processTransaction(txParams: any, cb): Promise<void> {
         const walletConnector = await this.getWalletConnector()
         try {
-          const result = await walletConnector.sendTransaction(txParams)
-          cb(null, result);
+            const result = await walletConnector.sendTransaction(txParams)
+            cb(null, result);
         } catch (error) {
-          cb(error);
+            cb(error);
         }
     }
 
     async processSignTransaction(txParams: any, cb): Promise<void> {
         const walletConnector = await this.getWalletConnector()
         try {
-          const result = await walletConnector.signTransaction(txParams)
-          cb(null, result);
+            const result = await walletConnector.signTransaction(txParams)
+            cb(null, result);
         } catch (error) {
-          cb(error);
+            cb(error);
         }
     }
 
-    async processMessage(msgParams: any, onAction: Promise<any>): Promise<void> {
-        // const result = await walletConnector.signMessage(msgParams)
+    async processMessage(msgParams: any, cb): Promise<void> {
+        const walletConnector = await this.getWalletConnector()
+        try {
+            const result = await walletConnector.signMessage(msgParams)
+            cb(null, result);
+        } catch (error) {
+            cb(error);
+        }
     }
 
-    async processPersonalMessage(msgParams: any, onAction: Promise<any>): Promise<void> {
-        // const result = await walletConnector.signPersonalMessage(msgParams)
-    }
-    
-    async processTypedMessage(msgParams: any, onAction: Promise<any>): Promise<void> {
-        // const result = await walletConnector.signTypedData(msgParams)
+    async processPersonalMessage(msgParams: any, cb): Promise<void> {
+        const walletConnector = await this.getWalletConnector()
+        try {
+            const result = await walletConnector.signPersonalMessage(msgParams)
+            cb(null, result);
+        } catch (error) {
+            cb(error);
+        }
     }
 
-    async getAccounts(onAction: Promise<any>): Promise<void> {
-        // const walletConnector = await engine.getWalletConnector()
-        // const accounts = walletConnector.accounts
-        // if (accounts && accounts.length) {
-        //   cb(null, accounts)
-        // } else {
-        //   cb(new Error('Failed to get accounts'))
-        // }
+    async processTypedMessage(msgParams: any, cb): Promise<void> {
+        const walletConnector = await this.getWalletConnector()
+        try {
+            const result = await walletConnector.signTypedData(msgParams)
+            cb(null, result);
+        } catch (error) {
+            cb(error);
+        }
+    }
+
+    async getAccounts(cb): Promise<void> {
+        const walletConnector = await this.getWalletConnector()
+        const accounts = walletConnector.accounts
+        if (accounts && accounts.length) {
+          cb(null, accounts)
+        } else {
+          cb(new Error('Failed to get accounts'))
+        }
     }
 
     async send(payload: any, callback: any): Promise<any> {
@@ -89,18 +110,22 @@ export class WalletConnectImpl implements IWalletConnectDappProvider {
             try {
                 await walletConnector.createSession();
                 if (this.qrcode) {
-                    // Use RxJS to convert to promise
-                    WalletConnectQRCodeModal.open(walletConnector.uri, () => {
-                        reject(new Error('User closed WalletConnect modal'))
-                    })
+                    const open = promisify(WalletConnectQRCodeModal.open);
+                    try {
+                    await open(walletConnector.uri);
+                    }
+                    catch (e) {
+                        throw new Error('User closed WalletConnect modal');
+                    }
                 }
                 // Use RxJS to convert to promise
-                walletConnector.on('connect', () => {
-                    if (this.qrcode) {
-                        WalletConnectQRCodeModal.close()
-                    }
-                    resolve(walletConnector)
-                })
+                const onConnect = fromEvent((walletConnector as any), 'connect')
+                .toPromise();
+                await onConnect;
+                if (this.qrcode) {
+                    WalletConnectQRCodeModal.close()
+                    return walletConnector;
+                }
             }
             catch (e) {
                 throw e;
